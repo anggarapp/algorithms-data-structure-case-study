2d756ab21902d96c672fed9d47181c290c8bb7ed
anggarapp <anggaraperpe@gmail.com>
2021-11-29 12:04:16 +0700

    add carry mechanism with unshift function

--- Javascript/number_increment_with_array_representation-recrusive.js
+++ Javascript/number_increment_with_array_representation-recrusive.js
@@ -2,18 +2,23 @@
 
 function incrementArray(numberArray, arrayLength = null) {
     lastIndex = (arrayLength === null) ? numberArray.length - 1 : arrayLength - 1;
-
+    resultArray = numberArray
     if (numberArray[lastIndex] === 9) {
         numberArray[lastIndex] = 0;
-        incrementArray(numberArray, lastIndex);
+        if (lastIndex <= 0) {
+            numberArray.unshift(1);
+        }
+        else {
+            incrementArray(numberArray, lastIndex);
+        }
     }
     else {
         numberArray[lastIndex] += 1;
     }
-
     return numberArray;
 }
 console.log(incrementArray([1]))
 console.log(incrementArray([1, 2]))
-console.log(incrementArray([9, 3, 9]))
+console.log(incrementArray([3, 9]))
 console.log(incrementArray([1, 9, 9]))
+console.log(incrementArray([9, 9, 9]))
