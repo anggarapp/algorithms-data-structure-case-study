a43878bcaece4d83825ed36ddf738fde120f1668
anggarapp <anggaraperpe@gmail.com>
2021-11-30 23:46:00 +0700

    add golang revese array

--- Go/reverse_array.go
+++ Go/reverse_array.go
@@ -2,11 +2,16 @@ package main
 import "fmt"
 
 func reverse_array(arr []int) []int {
-	return arr
+	var arr_length = len(arr)
+	var return_value = []int{}
+	for i := arr_length; i > 0; i-- {
+		return_value = append(return_value, arr[i-1])
+	}
+	return return_value
 }
 
 func main(){
-	var arr = []int{2, 3, 2, 4, 3}
+	var arr = []int{1,2,3,4}
 	fmt.Println(reverse_array(arr))
 
 }
